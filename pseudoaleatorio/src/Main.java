ff9eadec04711b72edcb94cc8ab49546e7315d61
jesustuz2000 <60972363+jesustuz2000@users.noreply.github.com>
2020-11-05 10:59:59 -0600

    Add files via upload

---
+++ pseudoaleatorio/src/Main.java
@@ -0,0 +1,13 @@
+
+/*
+* @author joset
+*/
+import gui.MainWindows;
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindows.getInstancia();
+    }
+    
+}
---
+++ pseudoaleatorio/src/logic/NumeroKS.java
@@ -0,0 +1,27 @@
+package logic;
+public class NumeroKS {
+
+	private double randomnumber;
+	private double accumulatedprovision; //distribucuion acumulada
+	private double difference;
+	
+	public double getRandomnumber() {
+		return randomnumber;
+	}
+	public void setRandomnumber(double randomnumber) {
+		this.randomnumber = randomnumber;
+	}
+	public double getAccumulatedprovision() {
+		return accumulatedprovision;
+	}
+	public void setAccumulatedprovision(double accumulatedprovision) {
+		this.accumulatedprovision = accumulatedprovision;
+	}
+	public double getDifference() {
+		return difference;
+	}
+	public void setDifference(double difference) {
+		this.difference = difference;
+	}
+	
+}
